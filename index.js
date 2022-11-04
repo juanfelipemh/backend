09297e063e42a8d4ce868a3e5ee01c48d6d10ef0
Juan Felipe Muñoz Hurtado <juanfelipemh@gmail.com>
2022-11-04 17:31:05 -0500

    update
    nuevo update bd

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 import express from 'express'
-import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import ProductoRouter from './routes/productoRouter.js';
 import bodyParser from 'body-parser';
 import cors from 'cors'
 import FacturasRouter from './routes/facturaRouter.js';
 import UsuarioRouter from './routes/usuarioRouter.js';
-
+import conectarDB from './config/db.js';
+const PORT = process.env.PORT || 4000;
 
 
 // Configuración a variables de entorno
 dotenv.config();
 
 
-//Conexión con BD
-mongoose.connect(process.env.MONGO).then( ()=>{
-    console.log("Conectado a la base de datos");
-}).catch((error) => {
-    console.log(error.message);
-})
-
 const app = express();
+app.use(express.json()) // Convertir String en objeto literal JSON
 
+//Conexión con BD
+conectarDB();
 
 // Middlewares
 const dominiosPermitidos = [process.env.FRONTEND_URL];
@@ -39,7 +35,7 @@ const corsOptions = {
 //app.use(cors(corsOptions)) 
 app.use(cors(corsOptions)) // Convierte la URL en una instrucción valida para axios
 
-app.use(express.json()) // Convertir String en objeto literal JSON
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
@@ -51,8 +47,6 @@ app.use("/api/usuarios", UsuarioRouter)
 
 
 // Conexión al puerto servidor
-const PORT = process.env.PORT || 8000;
-
 app.listen(PORT, () => {
     console.log(`Conectado al puerto ${PORT}`);
     
